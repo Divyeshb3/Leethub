9c8a5a07846772a124cf6630fe92eabd356bd18c
Divyesh Pichuka <154302830+Divyeshb3@users.noreply.github.com>
2026-08-03 23:34:09 +0530

    Time: 45 ms (86.04%), Space: 115.7 MB (77.89%) - LeetHub

---
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) 
+    {
+        Arrays.sort(intervals,(a,b)->a[1]-b[1]);
+        int c=0;
+        int prevend=intervals[0][1];
+        for(int i=1;i<intervals.length;i++)
+        {
+            if(prevend<=intervals[i][0])
+            {
+                prevend=intervals[i][1];
+            }
+            else
+            {
+                c++;
+            }
+        }
+        return c;
+    }
+}
