012a1e4d5b60c1e1ee15469291c08464288dec37
Divyesh Pichuka <154302830+Divyeshb3@users.noreply.github.com>
2026-08-11 22:56:00 +0530

    Time: 7 ms (6.45%), Space: 47.6 MB (86.07%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean validPalindrome(String s) 
+    {
+        int i=0;
+        int j=s.length()-1;
+        s = s.toLowerCase();
+        int onechance=0;
+        while(i<j)
+        {
+            if(s.charAt(i) == s.charAt(j))
+            {
+                i++;
+                j--;
+            }
+            else
+            {
+               return check(s,i+1,j)||check(s,i,j-1);         
+            }
+        }
+        return true;
+    }
+    public boolean check(String s,int i,int j)
+    {
+        while(i<j)
+        {
+            if(s.charAt(i)!=s.charAt(j))
+            {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
