34ab16df8b52702c62d50816ecd28f2d68edeaa1
Divyesh Pichuka <154302830+Divyeshb3@users.noreply.github.com>
2026-08-14 01:04:33 +0530

    Time: 2 ms (99.78%), Space: 45.4 MB (89.12%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,40 +1,53 @@
 class Solution {
     public String minWindow(String s, String t) 
     {
-        HashMap<Character,Integer> tmap=new HashMap<>();
-        HashMap<Character,Integer> smap=new HashMap<>();
-        int l=0;
-        int c=t.length();
-        int minlen=Integer.MAX_VALUE;
-        String ans="";
-        for(int i=0;i<t.length();i++)
+        int[] freq = new int[128];
+
+        for(int i = 0; i < t.length(); i++)
         {
-            tmap.put(t.charAt(i),tmap.getOrDefault(t.charAt(i),0)+1);
+            freq[t.charAt(i)]++;
         }
-        for(int r=0;r<s.length();r++)
+
+        int l = 0;
+        int count = t.length();
+
+        int minlen = Integer.MAX_VALUE;
+        int start = 0;
+
+        for(int r = 0; r < s.length(); r++)
         {
-            char ch=s.charAt(r);
-            smap.put(ch,smap.getOrDefault(ch,0)+1);
-            if(tmap.containsKey(ch)&&smap.get(ch)<=tmap.get(ch))
+            char ch = s.charAt(r);
+
+            if(freq[ch] > 0)
             {
-                c--;
+                count--;
             }
-            while(c==0)
+
+            freq[ch]--;
+
+            while(count == 0)
             {
-                if(r-l+1<minlen)
+                if(r - l + 1 < minlen)
                 {
-                    minlen=r-l+1;
-                    ans=s.substring(l,r+1);
+                    minlen = r - l + 1;
+                    start = l;
                 }
-                char leftchar=s.charAt(l);
-                smap.put(leftchar,smap.getOrDefault(leftchar,0)-1);
-                if(tmap.containsKey(leftchar) && smap.get(leftchar)<tmap.get(leftchar))
+
+                char left = s.charAt(l);
+
+                freq[left]++;
+
+                if(freq[left] > 0)
                 {
-                    c++;
+                    count++;
                 }
+
                 l++;
             }
         }
-        return ans;
+
+        return minlen == Integer.MAX_VALUE
+                ? ""
+                : s.substring(start, start + minlen);
     }
 }
